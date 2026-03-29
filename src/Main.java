356de3bfed8a549e07d93d9e0f31c2793454c9c9
Alec Giroir <Alecgiroir@gmail.com>
2026-03-29 12:24:19 -0600

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,66 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Random;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Magic Eight Ball");
+        String eightBallArt = "        .-\"\"\"\"-.\n" +
+                "      .'  .--.  '.\n" +
+                "     /   /    \\   \\\n" +
+                "    ;   |  ()  |   ;\n" +
+                "    |   |  /\\  |   |\n" +
+                "    ;   |  \\/  |   ;\n" +
+                "     \\   \\    /   /\n" +
+                "      '.  '--'  .'\n" +
+                "        '-.__.-'";
+        System.out.println(eightBallArt);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("What question would you like to ask the Oracle?");
+
+            String userInput = scanner.nextLine();
+
+            System.out.println("You asked...... " + userInput);
+
+            String eightBallAnswer = getMagic8BallAnswer();
+            System.out.println(eightBallAnswer);
+        }
+    }
+
+    private static String getMagic8BallAnswer() {
+        String[] magicEightBallResponses = {
+                "It is certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy, try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Don't count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful"
+        };
+
+        int lookupValue = getRandomLookup(magicEightBallResponses);
+        System.out.println("Lookup value: " + lookupValue);
+
+        return magicEightBallResponses[lookupValue];
+    }
+
+    private static int getRandomLookup(String[] responses) {
+        Random random = new Random();
+        return random.nextInt(responses.length);
+    }
 }
